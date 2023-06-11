29fb0291995ac98d2a7d68e753c06e3e93d49897
ahadvirus <ahadvirus@yahoo.com>
2023-06-11 12:37:19 +0330

    Commons files between class libraries

--- Libraries/Romak.Commons/Contracts/IModule.cs
+++ Libraries/Romak.Commons/Contracts/IModule.cs
@@ -1,5 +1,8 @@
 ﻿namespace Romak.Commons.Contracts;
 public interface IModule
 {
+    /// <summary>
+    /// The name of module (use for setting's name file in class library too)
+    /// </summary>
     string Name { get; }
 }
--- Libraries/Romak.Commons/Contracts/IStartup.cs
+++ Libraries/Romak.Commons/Contracts/IStartup.cs
@@ -1,6 +1,14 @@
-﻿namespace Romak.Commons.Contracts;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Romak.Commons.Contracts;
 
 public interface IStartup : IModule
 {
-    
+    /// <summary>
+    /// Adding new service to IoC
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/> The IoC of contain new service</param>
+    /// <param name="configuration"><see cref="IConfiguration"/> The configuration file base of module name</param>
+    void ConfigurationService(IServiceCollection services, IConfiguration configuration);
 }
