173b81f76a1309b58909578e93476245b25e25a8
ahadvirus <ahadvirus@yahoo.com>
2023-06-11 13:42:22 +0330

    Add two modules to solution

---
+++ Apps/Modules/Identity/Romak.Identity/Module.cs
@@ -0,0 +1,14 @@
+﻿using Romak.Commons.Contracts;
+
+namespace Romak.Identity;
+
+public class Module : IModule
+{
+    public string Name
+    {
+        get
+        {
+            return nameof(Identity);
+        }
+    }
+}
---
+++ Apps/Modules/Inventory/Romak.Inventory/Module.cs
@@ -0,0 +1,14 @@
+﻿using Romak.Commons.Contracts;
+
+namespace Romak.Inventory;
+
+public class Module : IModule
+{
+    public string Name
+    {
+        get
+        {
+            return nameof(Inventory);
+        }
+    }
+}
